6e543f5e5eb99faa1d994e8e4be3b881a728d776
Yhael <yhaelopez@gmail.com>
2022-01-28 16:00:16 -0700

    last changes

--- app/Http/Controllers/RQueryController.php
+++ app/Http/Controllers/RQueryController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Okvpn\R\Process\RProcess;
+use Okvpn\R\UnixPipes;
 
 class RQueryController extends Controller
 {
@@ -14,6 +16,29 @@ class RQueryController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //
+        try {
+
+            $rPath = '/usr/bin/R'; //path to R
+            $process = new RProcess(new UnixPipes(), $rPath);
+            $process->start();
+
+            $var = 10;
+
+            $rOutput = $process->write(<<<EOF
+                x = 1
+                y = 2
+                x + y
+                x + $var
+                x - y
+            EOF
+            );
+
+            // print_r(($rOutput->getAllOutput()));
+
+            dd($rOutput->getAllOutput());
+
+        } catch (\Throwable $th) {
+            dd($th);
+        }
     }
 }
